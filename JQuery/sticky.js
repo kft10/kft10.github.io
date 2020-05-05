6eaeefc2e1b917a40966d16c6cd1838388c96854
kft10 <59940646+kft10@users.noreply.github.com>
2020-05-05 17:01:04 -0400

    Updates

---
+++ JQuery/sticky.js
@@ -0,0 +1,23 @@
+DOCTYPE
+var yourNavigation = $(".nav");
+stickyDiv = "sticky";
+yourHeader = $(".header").height();
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > yourHeader) {
+    yourNavigation.addClass(stickyDiv);
+  } else {
+    yourNavigation.removeClass(stickyDiv);
+  }
+});
+var menuToggle = document.querySelector("#menu-toggle");
+var activeElements = document.querySelectorAll(".active-element");
+var toggledMenu = menuToggle.addEventListener("click", function(){
+     // forEach is not supported in IE11
+  // activeElements.forEach(function(e){
+  //     e.classList.toggle("active");
+  // });
+     for(var activated = 0; activated < activeElements.length; activated++){
+          activeElements[activated].classList.toggle("active");
+     }
+})
